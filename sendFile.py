53676682d533d7b5f1d3701d9445197f93291a70
wrenchomatic <Antti.kuismanen@abo.fi>
2022-02-24 13:04:24 +0200

    Modified picamera and sendfile code. Also created service files for automatic startup

---
+++ sendFile.py
@@ -0,0 +1,15 @@
+import os
+import paramiko
+import time
+
+
+while (True):
+    ssh = paramiko.SSHClient() 
+    ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+    ssh.connect("192.168.43.209", username="pi", password="rasp123berry!/")
+
+    sftp = ssh.open_sftp()
+    sftp.put("/home/pi/Desktop/picture.jpg", "/home/pi/Desktop/project/masters-thesis-project/djangoWebApp/media/picture.jpg")
+    sftp.close()
+    ssh.close()
+    time.sleep(30)
