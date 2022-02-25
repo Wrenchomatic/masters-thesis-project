677023ab01f172c23e098dfcce6d7caf4de9a3cf
wrenchomatic <Antti.kuismanen@abo.fi>
2022-02-25 14:29:28 +0200

    Fixed file IO problem in sensehat script

---
+++ copy-csv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script is meant to copy sensordata csv file
+
+while :
+do
+    cp /home/pi/Desktop/sensor_data.csv /home/pi/Desktop/project/masters-thesis-project/djangoWebApp/static
+    sleep 1m
+done
